63085518ec5332fae4be6b10bf303c14f5800530
SeDs <seds.admin@gmail.com>
2022-10-09 17:42:45 +0200

    Update registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,37 +3,46 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\SerializationContext;
 
-class UserController extends AbstractController
+class RegistrationController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $em,
+        private SerializerInterface $serializer,
     ){}
     
-    #[Route('/api/register', name: 'register', methods: ['POST'])]
-    public function register( Request $request, UserPasswordEncoderInterface $encoder ): JsonResponse
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register( Request $request, UserPasswordHasherInterface $passwordHasher ): JsonResponse
     {
-        $user = new User();
         $newUser = $this->serializer->deserialize($request->getContent(), User::class, 'json');
         $email = $newUser->getEmail();
-        $password = $newUser->getPassword();
+        $plaintextPassword = $newUser->getPassword();
         
-        if ( empty($password) || empty($email) ) {
+        if ( empty($email) || empty($plaintextPassword) ) {
             return $this->respondValidationError("Invalid Password or Email");
         }
-        $user->setEmail($email);
-        $user->setPassword($encoder->encodePassword($user, $password));
-        $this->em->persist($user);
-        $this->em-> flush();
         
-        return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+        $user = new User();
+        $user->setEmail($email);
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($passwordHasher->hashPassword($user, $hashedPassword));
+        $this->em->persist($user);
+        $this->em->flush();
+        
+        return new JsonResponse('User successfully created', Response::HTTP_OK, [], true);
     }
 }
